400ef7bc63bddd139709ec45f4dda3c55ca4486b
Michał Weskida <michal.weskida@gmail.com>
2022-08-19 13:59:07 +0200

    feat: added dependencies of libraries to applications

--- applications/application-1/index.js
+++ applications/application-1/index.js
@@ -1,3 +1,7 @@
+import library1Variable from '@mhal007/monorepo-lerna-library-1'
+
+console.log('library-1 variable value is ' + library1Variable)
+
 console.log('Hello World from application-1');
 
 const changeThisToTriggerNewVersion = 2;
--- applications/application-2/index.js
+++ applications/application-2/index.js
@@ -1,3 +1,9 @@
+import library1Variable from '@mhal007/monorepo-lerna-library-1'
+import library2Variable from '@mhal007/monorepo-lerna-library-2'
+
+console.log('library-1 variable value is ' + library1Variable)
+console.log('library-2 variable value is ' + library2Variable)
+
 console.log('Hello World from application-2');
 
 const changeThisToTriggerNewVersion = 2;
